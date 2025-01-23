332601df29c0ec5b7384ed2c1df5469c88171a93
JormaKuusYsi <onni.tamminiemi@gmail.com>
2025-01-23 13:42:10 +0200

    Fix error "hardware_interface::SystemInterface does not exist"

--- franka_gazebo/launch/computed_torque_controller.launch.py
+++ franka_gazebo/launch/computed_torque_controller.launch.py
@@ -21,29 +21,16 @@
 # SOFTWARE.
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, RegisterEventHandler
-from launch.conditions import IfCondition, UnlessCondition
-from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
-def launch_setup(context, *args, **kwargs):
-    """
-    Set up the launch configuration and nodes for the computed torque controller.
-
-    Returns:
-        list: A list of nodes to start.
-    """
+def generate_launch_description():
     # Initialize Arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
-    controllers_file = LaunchConfiguration("controllers_file")
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
-    computed_torque_controller_spawner = LaunchConfiguration("computed_torque_controller")
     initial_joint_controllers = PathJoinSubstitution(
-        [FindPackageShare(runtime_config_package), "config", controllers_file]
+        [FindPackageShare("franka_gazebo"), "config", "franka_controllers.yaml"]
     )
 
     # Find the robot description file
@@ -52,7 +39,9 @@ def launch_setup(context, *args, **kwargs):
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
+                [FindPackageShare("franka_description"), 
+                "urdf",
+                "effort_panda_arm.urdf.xacro"]
             ),
             " ",
             "sim_ignition:=true",
@@ -69,12 +58,13 @@ def launch_setup(context, *args, **kwargs):
         executable="robot_state_publisher",
         output="both",
         parameters=[{"use_sim_time": True}, robot_description],
+        remappings=[("robot_description", "robot_description")]
     )
 
     controller_manager_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, initial_joint_controllers],
+        parameters=[initial_joint_controllers],
         output="screen",
     )
 
@@ -113,90 +103,13 @@ def launch_setup(context, *args, **kwargs):
         launch_arguments={"gz_args": " -r -v 1 empty.sdf"}.items(),
     )
 
-    # Event handlers to ensure controller_manager is ready before spawners
-    delay_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=controller_manager_node,
-            on_exit=[joint_state_broadcaster_spawner],
-        )
-    )
-
-    # Event handlers to ensure controller is ready before spawners
-    delay_computed_torque_controller_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=controller_manager_node,
-            on_exit=[computed_torque_controller_spawner],
-        )
-    )
-
-    # Event handler to ensure Gazebo simulation is ready before launching the entity
-    delay_gz_launch = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=controller_manager_node,
-            on_exit=[gz_launch_description],
-        )
-    )
-    
-    # Event handler to ensure Gazebo spawner is ready before spawning the entity
-    delay_gz_spawn_entity = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=controller_manager_node,
-            on_exit=[gz_spawn_entity],
-        )
-    )
-
     nodes_to_start = [
         robot_state_publisher_node,
         controller_manager_node,
-        delay_joint_state_broadcaster_spawner,
-        delay_computed_torque_controller_spawner,
-        delay_gz_spawn_entity,
-        delay_gz_launch,
+        joint_state_broadcaster_spawner,
+        computed_torque_controller_spawner,
+        gz_spawn_entity,
+        gz_launch_description,
     ]
 
-    return nodes_to_start
-
-def generate_launch_description():
-    """
-    Generate the launch description for the computed torque controller.
-
-    This function declares the launch arguments and returns a LaunchDescription
-    object that includes the necessary nodes and event handlers for launching
-    the computed torque controller in a simulated environment.
-    """
-    declared_arguments = [
-        DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="franka_gazebo",
-            description='Package with the controller\'s configuration in "config" folder. \
-        Usually the argument is not set, it enables use of a custom setup.',
-        ),
-        DeclareLaunchArgument(
-            "controllers_file",
-            default_value="franka_controllers.yaml",
-            description="YAML file with the controllers configuration.",
-        ),
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="franka_description",
-            description="Description package with robot URDF/XACRO files. Usually the argument \
-        is not set, it enables use of a custom description.",
-        ),
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="effort_panda_arm.urdf.xacro",
-            description="URDF/XACRO description file with the robot with the effort interface.",
-        ),
-        DeclareLaunchArgument(
-            "computed_torque_controller",
-            default_value="computed_torque_controller",
-            description="Robot controller",
-        ),
-        DeclareLaunchArgument(
-            "sim_ignition",
-            default_value="true",
-            description="Sim ignition as physics engine",
-        ),
-    ]
-
-    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
+    return LaunchDescription(nodes_to_start)
